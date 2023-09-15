aa5f8b61797fe52976be18ff95c7eebc1d2e8e01
Ravivarman16 <129171351+Ravivarman16@users.noreply.github.com>
2023-09-16 00:33:56 +0530

    script to install java and docker on test server

---
+++ test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+apt-get update
+apt-get install -y openjdk-11-jre
+
+apt-get update
+apt-get upgrade -y
+apt-get install -y docker.io
+apt-get update
+
+systemctl enable docker
+systemctl start docker
