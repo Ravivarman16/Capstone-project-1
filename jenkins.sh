6eec41c7f80839a94d462a5ed8a16981236f8b28
Ravivarman16 <129171351+Ravivarman16@users.noreply.github.com>
2023-09-16 08:44:02 +0530

    this script contains Jenkins package to be installed Jenkins master

--- jenkins.sh
+++ jenkins.sh
@@ -3,7 +3,6 @@ apt-get update
 apt-get install -y openjdk-11-jre
 
 apt-get update
-apt-get upgrade -y
 
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
